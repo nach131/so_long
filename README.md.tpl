dde25d4867b368fc48ced42b5009698195dffb01
nacho <nacho.mota@gmail.com>
2022-11-24 22:13:44 +0100

    actions

---
+++ README.md.tpl
@@ -0,0 +1,17 @@
+<h1 align="center">
+  <img  width="120" alt="42logo"  src="https://user-images.githubusercontent.com/19689770/129336866-169b0dc7-ea41-47d4-b50a-d466508031af.png">
+  
+	get_next_line
+</h1>
+ <p align="center">
+<img alt="total-views" src="https://img.shields.io/badge/views-total_views-blue">
+<img alt="total-clone" src="https://img.shields.io/badge/clone-total_clone-blue">
+<img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/nach131/get_next_line">
+<img alt="GitHub top language" src="https://img.shields.io/github/languages/top/nach131/get_next_line">
+<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/nach131/get_next_line">
+<img alt="license-MIT" src="https://img.shields.io/badge/license-MIT-blue">
+</p>
+
+<h1 align="center">
+
+<!-- ![libft](https://github.com/nach131/42Barcelona/blob/main/images/125.png) -->
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs').promises
+const axios = require('axios');
+require('dotenv').config()
+
+async function getData(url){
+
+	var config = {
+		method: 'get',
+		url: url,
+		headers: { 
+			'Authorization': process.env.SECRET_KEY
+		}
+	};
+	const res = await axios(config)
+  // console.log('after the call to service');
+	// console.log(res.data.count)
+  return (res.data.count);
+}
+
+(async()=>{
+	const views = await getData('https://api.github.com/repos/nach131/get_next_line/traffic/views');
+	const clone = await getData('https://api.github.com/repos/nach131/get_next_line/traffic/clones');
+	const MarkdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
+
+	// console.log(views,clone)
+	const newMarkdown = MarkdownTemplate
+	.replace('total_views', views)
+	.replace('total_clone', clone)
+ 	// console.log(newMarkdown);
+	await fs.writeFile('./README.md', newMarkdown)
+})();
+
+
