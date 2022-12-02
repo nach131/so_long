dcd090c7d173597780fc931914b0598d676a2863
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-12-02 16:58:30 +0100

    cosas

--- sources/inc/so_long.h
+++ sources/inc/so_long.h
@@ -1,1 +1,11 @@
-#include <fcntl.h>
+#ifndef SO_LONG
+#define SO_LONG
+
+#include "../libft/inc/libft.h"
+#include "../libft/inc/colors.h"
+#include "../libft/inc/ft_printf.h"
+#include "../libft/inc/error.h"
+
+void arr_to_lst(void);
+
+#endif
