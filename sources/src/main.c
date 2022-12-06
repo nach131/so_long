da1a85e5039021903894b6cd182b3a48595fecdc
nacho <nacho.mota@gmail.com>
2022-12-06 01:14:32 +0100

    tengo 11 leaks en map

--- sources/src/main.c
+++ sources/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:13:28 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/12/05 23:16:42 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/12/06 01:13:49 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,32 @@
 
 #include "so_long.h"
 
-void print_arr(char **s)
+void print_arr_map(t_map *map)
 {
 	int i = 0;
-	while (s[i])
+
+	if (map->map)
 	{
-		ft_printf("%s", s[i]);
-		i++;
+		while (map->map[i])
+		{
+			ft_printf("%s", map->map[i]);
+			i++;
+		}
 	}
 }
 
 void ft_free_map(t_map *map)
 {
-	// int i = 0;
-	// while (map->map[i])
-	// {
-	// 	// map->map[i] = NULL;
-	// 	// free(map->map[i]);
-	// 	i++;
-	// }
+	int i = 0;
+
+	while (i < map->rows)
+	{
+		map->map[i] = NULL;
+		free(map->map[i]);
+		i++;
+	}
+	map->rows = 0;
+	map->cols = 0;
 	map->map = NULL;
 	free(map->map);
 }
@@ -44,10 +51,12 @@ int main(int argc, char **argv)
 	t_game game;
 
 	err_file(argc, argv[1]);
-
 	open_map(argv[1], &game.map);
 	ft_printf("\nrows:%d, cols:%d\n", game.map.rows, game.map.cols);
-	print_arr(game.map.map);
-	ft_free_map(&game.map);
-	// print_arr(game.map.map);
+	print_arr_map(&game.map);
+	ft_free_map(&game.map); // 11 leaks
+	// game.map.map = NULL;
+	// free(game.map.map); // 10 leaks
+
+	print_arr_map(&game.map);
 }
