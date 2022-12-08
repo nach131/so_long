dcf3298f3a44b34a56e1f3aa0c32939e8f9af02e
nacho <nacho.mota@gmail.com>
2022-12-08 19:46:20 +0100

    windows with one rocks

--- sources/src/main.c
+++ sources/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:13:28 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/12/08 16:26:07 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/12/08 19:43:03 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include "so_long.h"
-#include "../mlx/mlx.h"
+#include "mlx.h"
 
 void print_arr_map(t_map *map)
 {
@@ -53,12 +53,19 @@ int main(int argc, char **argv)
 
 	void *mlx_tmp;
 	void *win_tmp;
+	char *relative_path = "xpm/rocks.xpm";
+	void *img;
+	int img_width;
+	int img_height;
 
 	err_file(argc, argv[1]);
 	open_map(argv[1], &game.map);
 	// read_map(&game.map);
 	mlx_tmp = mlx_init();
 	win_tmp = mlx_new_window(mlx_tmp, 640, 480, "nach131 So Long");
+	img = mlx_xpm_file_to_image(mlx_tmp, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx_tmp, win_tmp, img, img_width, img_height);
+	mlx_loop(mlx_tmp);
 
 	ft_printf("\nrows:%d, cols:%d\n", game.map.rows, game.map.cols);
 	print_arr_map(&game.map);
